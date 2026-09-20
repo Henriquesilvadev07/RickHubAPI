a931faaa1494dc4963e5ceba4e9ac4c83e0355ed
RickDev <henriiisilva2007@gmail.com>
2026-09-20 19:41:17 -0300

    Instanciando DTO e model no metodo de salvar no teste unitario

--- src/test/java/com/rickdev/DevTasks/Service/TaskServiceTest.java
+++ src/test/java/com/rickdev/DevTasks/Service/TaskServiceTest.java
@@ -1,11 +1,18 @@
 package com.rickdev.DevTasks.Service;
 
+import com.rickdev.DevTasks.Dto.TaskDto;
+import com.rickdev.DevTasks.Model.StatusEnum;
+import com.rickdev.DevTasks.Model.TaskModel;
 import com.rickdev.DevTasks.Repository.TaskRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -21,6 +28,21 @@ class TaskServiceTest {
     @BeforeEach
     void setup() {
         MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    @DisplayName("Should salvar task successfully")
+    void salvar() {
+        TaskDto dto = new TaskDto("Limpeza",
+                "Limpar os PC",
+                StatusEnum.A_FAZER,
+                LocalDateTime.now());
+
+        TaskModel task = new TaskModel();
+        task.setTitulo(dto.titulo());
+        task.setDescricao(dto.descricao());
+        task.setStatus(dto.status());
+        task.setDataCriacao(dto.dataCricacao());
     }
 
 }
