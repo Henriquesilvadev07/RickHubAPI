85a5bee5988cf6f0b65536aeb2b40f6a350cc938
RickDev <henriiisilva2007@gmail.com>
2026-09-20 19:27:44 -0300

    Mock da repository e da classe service

--- src/test/java/com/rickdev/DevTasks/Service/TaskServiceTest.java
+++ src/test/java/com/rickdev/DevTasks/Service/TaskServiceTest.java
@@ -1,7 +1,26 @@
 package com.rickdev.DevTasks.Service;
 
+import com.rickdev.DevTasks.Repository.TaskRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class TaskServiceTest {
+
+    @Mock
+    private TaskRepository taskRepository;
+
+    @Mock
+    private TaskService taskService;
+
+    @BeforeEach
+    void setup() {
+        MockitoAnnotations.initMocks(this);
+    }
 
 }
